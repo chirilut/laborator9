0aa12d8bf28ad519eafe44f1917ac52bf82f6365
chirilut <chirilut_mihai@uahoo.com>
2020-01-05 23:22:39 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+                //ex1:
+                System.out.println("Hello");
+                System.out.println("Mihai Chirilut");
+
+                //ex2:
+                int a = 15;
+                double b = -1.5;
+                double sum= a+b;
+                System.out.println("ex2:  "  +sum);
+
+                //ex3:
+                int c = 10;
+                int d = 3;
+                float impartire = (float)c/d;
+                System.out.println("ex3:  " +impartire);
+
+                //ex 4 :
+                //4a:
+                int e= -5+8*6;
+                System.out.println("ex4a:  " +e);
+                //4b:
+                int f = (55+9)%9;
+                System.out.println("ex4b:  " +f);
+                //4c:
+                float g = (float)20 +-3*5 / 8;
+                ///rezultatul:g=18.125!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                System.out.println("ex4c:  " +g);
+                //4d:
+                float h = 5+15/3*2-8%3;
+                System.out.println("ex4d:  " +h);
+
+            }
+        }
